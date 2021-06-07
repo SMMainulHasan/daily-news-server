9bbc21d282fcd97192ace04ba0dd7ceba6d3b68c
Mainul Hasan <76752500+SMMainulHasan@users.noreply.github.com>
2021-06-07 23:48:46 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cxq2v.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const port = 8080;
+
+//Middleware
+app.use(cors());
+
+client.connect((err) => {
+  const blogCollection = client.db("dailyNews").collection("news");
+
+  app.get("/blogs", (req, res) => {
+    blogCollection.find({}).toArray((err, documents) => {
+      shuffleArray(documents);
+      res.send(documents);
+    });
+  });
+});
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+app.listen(process.env.PORT || port);
